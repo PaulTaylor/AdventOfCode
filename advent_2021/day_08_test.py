26393936c5a35fd5fb244bd718446fcbcbdf7924
Paul Taylor <184124+PaulTaylor@users.noreply.github.com>
2021-12-08 21:27:03 +0000

    completed day 08

---
+++ advent_2021/day_08_test.py
@@ -0,0 +1,29 @@
+import pytest
+
+from .day_08 import parse_input, part_a, part_b
+
+@pytest.fixture
+def string_input():
+    return """be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce"""
+
+def test_part_a(string_input):
+    patterns = parse_input(string_input)
+    assert len(patterns) == 10
+    assert len(patterns[0]) == 2
+    assert len(patterns[0][0]) == 10
+    assert len(patterns[0][1]) == 4
+
+    assert part_a(patterns) == 26
+
+def test_part_b(string_input):
+    patterns = parse_input(string_input)
+    assert part_b(patterns) == 61229
